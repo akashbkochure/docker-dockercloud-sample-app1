877a9e76aba56428fc93efab396a90132ee8f968
Nigel Poulton <Nigel Poulton>
2016-02-27 16:49:14 +0000

    adding version
    Signed-off-by: Nigel Poulton <Nigel Poulton>

--- www/index.php
+++ www/index.php
@@ -19,6 +19,7 @@
 	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
+	<b>Version: v2</b>
 	<?php
 	$links = [];
 	foreach($_ENV as $key => $value) {
